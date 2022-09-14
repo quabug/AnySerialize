6abd24efe230e4513a4da00732e2818c58014061
quabug <quabug@gmail.com>
2022-09-14 22:08:13 +0800

    fix multi-dim list (#24)

---
+++ Packages/com.quabug.any-serialize/Runtime/ReadOnlyAnyList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace AnySerialize
+{
+    [Serializable, AnySerializePriority(5000)]
+    public class ReadOnlyAnyList<T, [AnyConstraintType] TAny> : IReadOnlyAny<List<T>> where TAny : IReadOnlyAny<T>
+    {
+        [SerializeField] private List<TAny> _value = default!;
+        private List<T> _cache = new();
+        public List<T> Value
+        {
+            get
+            {
+#if !UNITY_EDITOR
+                if (_cache.Count == _value.Count) return _cache;
+#endif
+                _cache.Clear();
+                _cache.AddRange(_value.Select(v => v.Value));
+                return _cache;
+            }
+        }
+    }
+}
