d26782ad0011b2af56eaba4bae50e4d48089ec4c
quabug <quabug@gmail.com>
2022-09-14 15:31:38 +0800

    drawers

---
+++ Packages/com.quabug.any-serialize/Editor/AnySingleValueDrawer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace AnySerialize.Editor
+{
+    [CustomPropertyDrawer(typeof(AnyValue<>), true)]
+    [CustomPropertyDrawer(typeof(ReadOnlyAnyArray<,>))]
+    [CustomPropertyDrawer(typeof(ReadOnlyAnyLazy<,>))]
+    public class AnySingleValueDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            property.NextVisible(enterChildren: true);
+            return EditorGUI.GetPropertyHeight(property, label, includeChildren: true);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            property.NextVisible(enterChildren: true);
+            EditorGUI.PropertyField(position, property, label, includeChildren: true);
+        }
+    }
+}
