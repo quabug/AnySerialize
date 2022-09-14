6a816251b690562a1ebb0eb22ad13ef6de298277
quabug <quabug@gmail.com>
2022-09-14 15:50:46 +0800

    compare priority before check type distance

--- Packages/com.quabug.any-serialize/CodeGen/Extension.cs
+++ Packages/com.quabug.any-serialize/CodeGen/Extension.cs
@@ -9,26 +9,25 @@ namespace AnySerialize.CodeGen
 {
     public static class Extension
     {
-        public static TypeReference CreateConcreteTypeFrom(this Container container, TypeReference type)
+        public static TypeReference? CreateConcreteTypeFrom(this Container container, TypeReference type)
         {
             var def = type.Resolve();
-            if (def.IsAbstract) return null;
-            if (def.GetConstructors().FirstOrDefault(ctor => !ctor.HasParameters) == null) return null;
+            if (def == null || def.IsAbstract || def.GetConstructors()!.FirstOrDefault(ctor => !ctor.HasParameters) == null) return null;
             if (type.IsConcreteType()) return type;
             
-            if (!(type is GenericInstanceType genericType))
+            if (type is not GenericInstanceType genericType)
                 throw new ArgumentException($"{nameof(type)}({type}) must be a {nameof(GenericInstanceType)}", nameof(type));
             
-            var anyGenericParameterSearcherAttributeType = type.Module.ImportReference(typeof(IAnyGenericParameterSearcherAttribute));
+            var anyGenericParameterSearcherAttributeType = type.Module!.ImportReference(typeof(IAnyGenericParameterSearcherAttribute))!;
                     
-            for (var i = 0; i < genericType.GenericArguments.Count; i++)
+            for (var i = 0; i < genericType.GenericArguments!.Count; i++)
             {
-                var arg = genericType.GenericArguments[i];
+                var arg = genericType.GenericArguments[i]!;
                 if (arg is GenericParameter parameter)
                 {
                     if (!parameter.HasCustomAttributes) return null;
-                    var attribute = parameter.CustomAttributes
-                        .FirstOrDefault(attr => attr.AttributeType.IsDerivedFrom(anyGenericParameterSearcherAttributeType))
+                    var attribute = parameter.CustomAttributes!
+                        .FirstOrDefault(attr => attr.AttributeType!.IsDerivedFrom(anyGenericParameterSearcherAttributeType))
                     ;
                     if (attribute == null) return null;
                     var genericArgument = container.Search(
