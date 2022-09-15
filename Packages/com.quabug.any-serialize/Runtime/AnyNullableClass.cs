41928a2933ad9971f25404a2b2605d9e593cffed
quabug <quabug@gmail.com>
2022-09-15 23:36:26 +0800

    any nullable class

---
+++ Packages/com.quabug.any-serialize/Runtime/AnyNullableClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace AnySerialize
+{
+    [Serializable, AnySerializePriority(20000)]
+    public class AnyNullableClass<T> : IReadOnlyAny<T?>, IAny<T?> where T : class
+    {
+        [field: SerializeReference] public T? Value { get; set; } = null;
+    }
+}
