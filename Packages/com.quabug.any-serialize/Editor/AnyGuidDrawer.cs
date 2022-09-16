f7b06e3aeaae7d583b7193948031c18768a75767
quabug <quabug@gmail.com>
2022-09-16 17:24:30 +0800

    guid

---
+++ Packages/com.quabug.any-serialize/Editor/AnyGuidDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace AnySerialize.Editor
+{
+    [CustomPropertyDrawer(typeof(AnyGuid))]
+    public class AnyGuidDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            property.NextVisible(enterChildren: true);
+            return EditorGUI.GetPropertyHeight(property, label, includeChildren: true);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            property.NextVisible(enterChildren: true);
+            var buttonWidth = 40;
+            var propertyPosition = new Rect(position.x, position.y, position.width - buttonWidth * 1.5f, position.height);
+            var buttonPosition = new Rect(position.width - buttonWidth, position.y, buttonWidth, position.height);
+            EditorGUI.PropertyField(propertyPosition, property, label, includeChildren: true);
+            if (GUI.Button(buttonPosition, "new")) property.stringValue = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ Packages/com.quabug.any-serialize/Runtime/AnyGuid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace AnySerialize
+{
+    [Serializable]
+    public class AnyGuid : IReadOnlyAny<Guid>, IAny<Guid>
+    {
+        [SerializeField] private string _guid;
+
+        public Guid Value
+        {
+            get => Guid.Parse(_guid);
+            set => _guid = value.ToString();
+        }
+    }
+}
